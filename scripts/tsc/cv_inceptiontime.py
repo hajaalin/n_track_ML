24dd4922d1ef79e16c7763a616b8d4455ee5267e
Harri Jäälinoja <harri.jaalinoja@helsinki.fi>
2022-11-22 15:56:48 +0200

    - remove method inceptiontime_cv (one cv run) - remove method prepare_data_for_inception - general cleanup

--- scripts/tsc/cv_inceptiontime.py
+++ scripts/tsc/cv_inceptiontime.py
@@ -1,12 +1,17 @@
+import click
+from datetime import datetime
 import logging
-from pathlib import Path
 import numpy as np
-import shap
+from pathlib import Path
+import pandas as pd
 from scikeras.wrappers import KerasClassifier
-import sklearn
+import shap
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.preprocessing import OneHotEncoder
+import sys
 import tensorflow as tf
+import time
 # https://stackoverflow.com/questions/66814523/shap-deepexplainer-with-tensorflow-2-4-error
 #from tensorflow.compat.v1.keras.backend import get_session
 #tf.compat.v1.disable_v2_behavior() 
@@ -14,55 +19,8 @@ import tensorflow as tf
 from utility import parse_config
 from etl_tsc import load_data, fsets
 
+
 logger = logging.getLogger(__name__)
-
-
-'''
-This method was copied and modified from 'prepare_data' in InceptionTime/main.py:
-https://github.com/hajaalin/InceptionTime/blob/f3fd6c5e9298ec9ca5d0fc594bb07dd1decc3718/main.py#L15
-'''
-def prepare_data_for_inception(X,y):
-    # use all data, use cross-validation
-    x_train = X.copy()
-    y_train = y.copy()
-
-    nb_classes = len(np.unique(np.concatenate((y_train,), axis=0)))
-
-    # make the min to zero of labels
-    #y_train, y_test = transform_labels(y_train, y_test)
-
-    # save orignal y because later we will use binary
-    y_true_train = y_train.astype(np.int64)
-    # transform the labels from integers to one hot vectors
-    enc = sklearn.preprocessing.OneHotEncoder()
-    enc.fit(np.concatenate((y_train,), axis=0).reshape(-1, 1))
-    y_train = enc.transform(y_train.reshape(-1, 1)).toarray()
-
-    if len(x_train.shape) == 2:  # if univariate
-        # add a dimension to make it multivariate with one dimension
-        x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
-
-    return x_train, y_train, nb_classes, y_true_train, enc
-
-
-
-
-def standard_scale_x_by_series(_X):
-    scaled = np.ndarray(_X.shape)
-
-    # loop over samples
-    for i in range(_X.shape[0]):
-        # loop over features
-        for j in range(_X.shape[1]):
-            # calculate mean and std for the time series
-            u = np.mean(_X[i][j])
-            s = np.std(_X[i][j])
-            # scale the time series
-            scaled[i][j] = (_X[i][j] - u) / s
-            #print(np.mean(scaled[i][j]))
-            #print(np.std(scaled[i][j]))
-
-    return scaled
 
 
 def get_standard_scaling(X):
@@ -111,127 +69,6 @@ def apply_standard_scaling(X,mean,std):
 ## But what is it about that split that makes it so successful?
 TARGET_ACCURACY = 0.98
 
-'''
-Single cross-validation run
-'''
-def inceptiontime_cv(cv, X_inc, y_inc, y_true, groups, output_it, \
-                     kernel_size, epochs=250, feature_names=None, nb_classes=2, return_model_eval=False, save_shap_values=False):
-    output_directory = output_it
-    input_shape = X_inc.shape[1:]
-    verbose = False
-
-    from classifiers import inception
-    def create_model():
-        classifier_keras = inception.Classifier_INCEPTION(output_directory, input_shape, nb_classes, \
-                                                      kernel_size=kernel_size, nb_epochs=epochs, \
-                                                      verbose=verbose)
-        #print(classifier_keras.model)
-        #classifier_keras.model.summary()
-        return classifier_keras.model
-
-    batch_size = int(min(X_inc.shape[0] / 10, 16))
-    columns = ['accuracy','precision','recall','f1']
-    scores = pd.DataFrame(columns=columns)
-
-    shap_lists = []
-    shap_deep_list = []
-    shap_grad_list = []
-    X_test_list = []
-    accuracy_list = []
-    train_index_list = []
-    
-    # One-hot encoding is a problem for StratifiedGroupKFold,
-    # split using y_true
-    for train_index,val_index in cv.split(X_inc,y_true,groups):
-        #print('cv loop')
-        #print(train_index.shape)
-        #print(X_inc[train_index].shape)
-        input_shape = X_inc[train_index].shape[1:]
-        #print(input_shape)
-
-        #print(train_index)
-        #print(val_index)
-        #continue
-        #print(y_true[train_index])
-        #print(y_inc[train_index])
-        #break
-
-        # scale training data to mean,std 0,1
-        #X_train_scaled = standard_scale_x_by_series(X_inc[train_index])
-        mean,std = get_standard_scaling(X_inc[train_index])
-        logger.debug("mean:")
-        logger.debug(mean)
-        logger.debug("std:")
-        logger.debug(std)
-        X_train_scaled = apply_standard_scaling(X_inc[train_index],mean,std)
-        #print(X_train_scaled.shape)
-        #logger.debug("scaled2")
-        #logger.debug(X_train_scaled)
-       
-        classifier = KerasClassifier(model=create_model(), \
-                                     epochs=epochs, \
-                                     batch_size=batch_size, \
-                                     verbose=verbose)
-        classifier.fit(X_train_scaled, y_inc[train_index])
-
-        # scale validation data to mean,std 0,1
-        #X_val_scaled = standard_scale_x_by_series(X_inc[val_index])
-        X_val_scaled = apply_standard_scaling(X_inc[val_index],mean,std)
-        pred = classifier.predict(X_val_scaled)
-
-        truth = y_true[val_index]
-
-        #print('truth')
-        #print(truth)
-        #print('pred')
-        #print(pred)
-
-        # prediction is onehot-encoded, reverse it
-        pred = pred.argmax(1)
-        #print(pred)
-
-        # get fold accuracy and append
-        fold_acc = accuracy_score(truth, pred)
-        fold_prc = precision_score(truth, pred)
-        fold_rec = recall_score(truth, pred)
-        fold_f1 = f1_score(truth, pred)
-        scores.loc[len(scores)] = [fold_acc,fold_prc,fold_rec,fold_f1]
-
-        model_eval = None
-        # if accuracy is close enough to target,
-        # save model and SHAP values
-        print(fold_acc)
-        if return_model_eval and abs(TARGET_ACCURACY - fold_acc) < 0.02:
-            print("sufficiently accurate model found")
-            (shap_deep, shap_grad) = get_shap_values(classifier.model_, \
-                                                     X_train_scaled, \
-                                                     X_val_scaled)
-    
-            model_eval = (classifier.model_, feature_names, shap_deep, shap_grad, X_val_scaled, pred, truth)
-            return pd.DataFrame(), model_eval, shap_lists
-
-        accuracy_list.append(fold_acc)
-        train_index_list.append(train_index)
-        if save_shap_values:
-            (shap_deep, shap_grad) = get_shap_values(classifier.model_, \
-                                                     X_train_scaled, \
-                                                     X_val_scaled)
-            shap_deep_list.append(shap_deep[1])
-            shap_grad_list.append(shap_grad[1])
-            X_test_list.append(X_val_scaled)
-
-               
-    scores['classifier'] = 'InceptionTime'
-    scores['kernel_size'] = kernel_size
-    scores['epochs'] = epochs
-
-    print("len(shap_deep_list):%d" % len(shap_deep_list))
-    print("len(shap_grad_list):%d" % len(shap_grad_list))
-    
-    shap_lists = (shap_deep_list, shap_grad_list, X_test_list, accuracy_list, train_index_list)
-
-    return scores, model_eval, shap_lists
-
 
 def get_shap_values(model_, X_train, X_test):
     # SHAP
@@ -248,23 +85,48 @@ def get_shap_values(model_, X_train, X_test):
     return (shap_values_deep, shap_values_grad)
    
 
+
 '''
 Repeat cross-validation
 '''
-def inceptiontime_cv_repeat(X, y, groups, features, output_it, fset, kernel_size=20, epochs=250, repeats=10,job_id='', return_model_eval=False, save_shap_values=False, set_split_random_state=False):
+def inceptiontime_cv_repeat(X, y, groups, features, output_it, fset, kernel_size=20, epochs=250, repeats=1,job_id='', save_shap_values=False, set_split_random_state=False):
     logger.info(fset)
+    logger.debug("X: " + str(X.shape))
+    logger.debug("y: " + str(y.shape))
 
     # fset includes also class and file, here they have been removed
     print(features)
 
-    # prepare_data_for inception returns all, no split to train and test sets
-    X_inc, y_inc, nb_classes, y_true, enc = prepare_data_for_inception(X,y)
+    nb_classes = 2
+    input_shape = X.shape[1:]
+    batch_size = int(min(X.shape[0] / 10, 16))
+    verbose = False
+    print("input_shape")
+    print(input_shape)
 
-    logger.debug("X_inc: " + str(X_inc.shape))
-    logger.debug("y_inc: " + str(y_inc.shape))
+    
+    # save original y 
+    y_true = y.astype(np.int64)
+    
+    # transform the labels from integers to one hot vectors
+    # https://github.com/hajaalin/InceptionTime/blob/f3fd6c5e9298ec9ca5d0fc594bb07dd1decc3718/main.py#L15
+    enc = OneHotEncoder()
+    enc.fit(np.concatenate((y,), axis=0).reshape(-1, 1))
+    y = enc.transform(y.reshape(-1, 1)).toarray()
 
-    scores_all = []
-    shap_lists_all = []
+    
+    # columns recorded for each round of cross-validation
+    columns = ['accuracy','precision','recall','f1','repeat']
+    scores = pd.DataFrame(columns=columns)
+
+    # containers for saving data for model evaluation
+    list_idx_train = []
+    list_idx_test = []
+    list_accuracy = []
+    list_shap_deep = []
+    list_shap_grad = []
+    list_X_test = []
+
     for i in range(repeats):
         if set_split_random_state:
             cv = StratifiedGroupKFold(n_splits=4, shuffle=True, random_state=42)
@@ -274,43 +136,123 @@ def inceptiontime_cv_repeat(X, y, groups, features, output_it, fset, kernel_size
         
         print('repeat: %d/%d' % (i+1, repeats))
         logger.debug('repeat: %d/%d' % (i+1, repeats))
-        cv_output = inceptiontime_cv(cv, X_inc, y_inc, y_true, \
-                                     groups, output_it, \
-                                     kernel_size=kernel_size, epochs=epochs, \
-                                     feature_names=features, \
-                                     nb_classes=nb_classes, \
-                                     return_model_eval=return_model_eval, \
-                                     save_shap_values=save_shap_values)
-        scores, model_eval, shap_lists = cv_output
-        shap_lists_all.append(shap_lists)
 
-        scores['repeat'] = i+1
-        scores_all.append(scores)
+        # This import will fail if placed in the beginning of the file.
+        # InceptionTime source directory must be added to sys.path first.
+        from classifiers import inception
+        def create_model():
+            clsfr = inception.Classifier_INCEPTION(output_it, \
+                                                   input_shape, \
+                                                   nb_classes, \
+                                                   kernel_size=kernel_size, \
+                                                   nb_epochs=epochs, \
+                                                   verbose=verbose)
+            #print(clsfr.model)
+            #clsfr.model.summary()
+            return clsfr.model
 
-        #print(return_model_eval)
-        #print(model_eval)
-        if return_model_eval and (model_eval != None):
-            return model_eval
+
+        # One-hot encoding is a problem for StratifiedGroupKFold,
+        # split using y_true
+        for train_index,val_index in cv.split(X,y_true,groups):
+            #print('cv loop')
+
+            #print(train_index)
+            #print(val_index)
+            #continue
+            #print(y_true[train_index])
+            #print(y_inc[train_index])
+            #break
+
+            # scale training data to mean,std 0,1
+            mean,std = get_standard_scaling(X[train_index])
+            logger.debug("mean:")
+            logger.debug(mean)
+            logger.debug("std:")
+            logger.debug(std)
+            X_train_scaled = apply_standard_scaling(X[train_index],mean,std)
+            print("check scaled shape")
+            print(X[train_index].shape)
+            print(X_train_scaled.shape)
+            #logger.debug("scaled2")
+            #logger.debug(X_train_scaled)
+
+            classifier = KerasClassifier(model=create_model(), \
+                                         epochs=epochs, \
+                                         batch_size=batch_size, \
+                                         verbose=verbose)
+            classifier.fit(X_train_scaled, y[train_index])
+
+            # scale validation data to mean,std 0,1
+            #X_val_scaled = standard_scale_x_by_series(X_inc[val_index])
+            X_val_scaled = apply_standard_scaling(X[val_index],mean,std)
+            pred = classifier.predict(X_val_scaled)
+
+            truth = y_true[val_index]
+
+            #print('truth')
+            #print(truth)
+            #print('pred')
+            #print(pred)
+
+            # prediction is onehot-encoded, reverse it
+            pred = pred.argmax(1)
+            #print(pred)
+
+            # get fold accuracy and append to dataframe
+            fold_acc = accuracy_score(truth, pred)
+            fold_prc = precision_score(truth, pred)
+            fold_rec = recall_score(truth, pred)
+            fold_f1 = f1_score(truth, pred)
+            scores.loc[len(scores)] = [fold_acc,fold_prc,fold_rec,fold_f1,i+1]
+
+            print("fold_acc %f0.00" % fold_acc)
+
+            list_accuracy.append(fold_acc)
+            list_idx_train.append(train_index)
+            list_idx_test.append(val_index)
+            if save_shap_values:
+                (shap_deep, shap_grad) = get_shap_values(classifier.model_, \
+                                                         X_train_scaled, \
+                                                         X_val_scaled)
+                list_shap_deep.append(shap_deep[1])
+                list_shap_grad.append(shap_grad[1])
+                list_X_test.append(X_val_scaled)
+
+        print("len(list_shap_deep):%d" % len(list_shap_deep))
+        print("len(list_shap_grad):%d" % len(list_shap_grad))
         
-    scores = pd.concat(scores_all)
-
+    # once accuracy metrics have been recorded,
+    # add columns with metadata
     scores['cv'] = str(cv)
+    scores['classifier'] = 'InceptionTime'
     scores['fset'] = fset
     scores['kernel_size'] = kernel_size
     scores['epochs'] = epochs
     scores['job_id'] = job_id
 
+
     logger.info(f"inceptiontime_cv_repeat: %s feature_set:%s, kernel_size:%d, epochs:%d, accuracy:%f0.00" % (str(cv), fset, kernel_size, epochs, scores['accuracy'].mean()))
 
-    print("len(shap_lists_all):%d" % len(shap_lists_all))
-    return scores, shap_lists_all
+    lists_all = (list_accuracy, list_idx_train, list_idx_test, \
+                 list_shap_deep, list_shap_grad, list_X_test)
+
+    return scores, lists_all
 
 
-import click
-from datetime import datetime
-import pandas as pd
-import sys
-import time
+def shap2npy(fset, shap_lists_all, output_shap):
+    (list_accuracy, list_idx_train, list_idx_test, \
+     list_shap_deep, list_shap_grad, list_X_test) = shap_lists_all
+
+    np.save(output_shap / 'list_idx_train.npy', list_idx_train)
+    np.save(output_shap / 'list_idx_test.npy', list_idx_test)
+    np.save(output_shap / 'list_accuracy.npy', list_accuracy)
+    np.save(output_shap / 'list_shap_deep.npy', list_shap_deep)
+    np.save(output_shap / 'list_shap_grad.npy', list_shap_grad)
+    np.save(output_shap / 'list_X_test.npy', list_X_test)
+    np.save(output_shap / 'features.npy', fsets[fset])
+        
+
 
 @click.command()
 @click.option("--inceptiontime_dir", type=str)
@@ -389,32 +331,6 @@ def cv_inceptiontime(inceptiontime_dir, paths, kernel_size, epochs, fset, repeat
     # save X_test and accuracy lists, even if SHAP values are not calculated
     shap2npy(fset, shap_lists_all, output_shap)
 
-
-def shap2npy(fset, shap_lists_all, output_shap):
-    shap_deep_list = []
-    shap_grad_list = []
-    X_test_list = []
-    accuracy_list = []
-    train_index_list = []
-
-    # loop over repetitions
-    for shap_lists in shap_lists_all:
-        # each repetition produced a tuple of lists
-        shapd, shapg, X_test, acc, train = shap_lists
-        
-        shap_deep_list.extend(shapd)
-        shap_grad_list.extend(shapg)
-        X_test_list.extend(X_test)
-        accuracy_list.extend(acc)
-        train_index_list.extend(train)
-
-    np.save(output_shap / 'shap_deep_list.npy', shap_deep_list)
-    np.save(output_shap / 'shap_grad_list.npy', shap_grad_list)
-    np.save(output_shap / 'X_test_list.npy', X_test_list)
-    np.save(output_shap / 'accuracy_list.npy', accuracy_list)
-    np.save(output_shap / 'train_index_list.npy', train_index_list)
-    np.save(output_shap / 'features.npy', fsets[fset])
-        
         
 
 if __name__ == "__main__":
